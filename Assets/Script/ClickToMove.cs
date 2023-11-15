23424df8e6c8bffc7c9b6ba57d89b09f6dfdd4d0
DomePuma <mathys.mechain@gmail.com>
2023-11-15 12:21:12 +0100

    NavMesh + Click Mouse to Move

--- Assets/Script/ClickToMove.cs
+++ Assets/Script/ClickToMove.cs
@@ -1,6 +1,24 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ClickToMove : MonoBehaviour
 {
-    
+    private Camera _mainCamera;
+    [SerializeField] private NavMeshAgent _agent;
+    private void Awake() 
+    { 
+        _mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);         
+            if(Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, 1 << 3))
+            {
+                _agent.SetDestination(hit.point);
+            }
+        }
+    }
 }
